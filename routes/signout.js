2fb2e2a413bce82ea0c9213672f36a1d071804cb
lemontree <gonelove.guo@gmail.com>
2017-11-22 22:52:42 +0800

    完成登录登出

--- routes/signout.js
+++ routes/signout.js
@@ -4,7 +4,9 @@ const router = express.Router();
 const checkLogin = require('../middlewares/check').checkLogin;
 
 router.get('/', checkLogin, function(req, res, next) {
-  res.send('登出');
+  req.session.user = null;
+  req.flash('success', '登出成功');
+  res.redirect('/posts');
 })
 
 module.exports = router;
